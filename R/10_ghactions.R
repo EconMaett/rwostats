5a8ab4eb00cf34205b22073331614efb1f775a0a
EconMaett <matthias.spichiger@bluewin.ch>
2024-03-23 15:25:51 +0100

    update

--- R/10_ghactions.R
+++ R/10_ghactions.R
@@ -36,6 +36,9 @@ survey_data |>
 
 # Note that you need to have a file "R/import-data.R"
 
+# Note that the directories have to be correctly specified.
+
+# Note that the github actions job will take some time to run!
 
 
 ## How GitHub Actions Work -----
@@ -46,12 +49,60 @@ survey_data |>
 # These YAML files contain a set of instructions that tell
 # a virtual computer what to do.
 
-
 # Be careful with spacing in YAML files.
 # Each indentation should be done with two spaces (not tabs).
 
+
 ## Triggers Start GitHub Actions ----
 
+# At the very top of the YAML file is the trigger.
+
+# on:
+#   push:
+#     branches: main
+
+# Whenever we push to the main branch, run this code.
+
+
+# The next section is the "jobs" line.
+# jobs:
+#   import-data: // specifies the name of the job
+#   runs-on: ubuntu-latest // specifies the operating system of the virtual machine
+#   steps:
+#     - name: Set up R
+#       users: r-lib/actions/setup-r@v2 // Pre-existing GitHub action to install R
+
+#     - name: Install packages
+#       uses: r-lib/actions/setup-r-dependencies@v2 // Set up any dependencies for these packages
+#       with:
+#         packages: |
+#           any::tidyverse
+#           any::googlesheets4
+
+#     - name: Check out repository
+#         uses: actions/checkout@v3 // Check out this repository on the virtual machine
+
+#     - name: Import data
+#       run: Rscript -e "source("R/import-data.R") // Run the code in this R script.
+
+#     - name: Commit results
+#       run: |
+#         git config --local user.email "matthias.spichiger@bluewin.ch"
+#         git config -- local user.name "EconMaett"
+#         git data/survey_data.rds
+#         git commit -m "Data updated" || echo "No changes to commit"
+#         git push origin || echo "No changes to commit" // Commit message and push to main branch or no changes
+
+# Note: The job can take 20 minutes to run on GitHub actions.
+# The first time you run the job will take the longest.
+
+# It took about 15 minutes in this case.
+
+# There should be a commit called "Data updated".
+
+# Fill in the survey to add some more data.
+
+# You can commit the file again on GitHub Desktop.
 
 ## Jobs Lay Out the Steps to Run When the Action is Triggered -----
 
